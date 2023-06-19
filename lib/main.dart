6eca2adc8559e61776d2b4744eb7a57d56cd45b1
PiyushKhairnar53 <piyushkhairnar53@gmail.com>
2023-06-19 16:32:28 +0530

    Splash screen created

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:get/get.dart';
+import 'package:task_management_app/views/screens/home_screen.dart';
+import 'package:task_management_app/views/screens/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,72 +12,35 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+
+    const Map<int, Color> color =
+    {
+      50:Color.fromRGBO(7,71,166, .1),
+      100:Color.fromRGBO(7,71,166, .2),
+      200:Color.fromRGBO(7,71,166, .3),
+      300:Color.fromRGBO(7,71,166, .4),
+      400:Color.fromRGBO(7,71,166, .5),
+      500:Color.fromRGBO(7,71,166, .6),
+      600:Color.fromRGBO(7,71,166, .7),
+      700:Color.fromRGBO(7,71,166, .8),
+      800:Color.fromRGBO(7,71,166, .9),
+      900:Color.fromRGBO(7,71,166, 1),
+    };
+    
+    String hexColor = "#0747a6";
+    // MaterialColor color = MaterialColor(0xFF000000,color);
+
+    return GetMaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: const MaterialColor(0xFF0747A6,color),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      initialRoute: "/splash",
+      getPages: [
+        GetPage(name: "/splash", page: () => const SplashScreen()),
+        GetPage(name: "/home", page: () => const HomeScreen()),
+      ],
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  final myController = TextEditingController();
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: TextFormField(
-                controller: myController,
-                decoration: const InputDecoration(
-                  border: UnderlineInputBorder(),
-                  labelText: 'Enter username',
-                ),
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () async {
-
-                final prefs = await SharedPreferences.getInstance();
-                prefs.setString('num',myController.text);
-                print(prefs.getString('num')!);
-
-              },
-              child: const Padding(
-                padding: EdgeInsets.fromLTRB(10, 5, 10, 5),
-                child: Text("Save"),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
---
+++ lib/views/screens/home_screen.dart
@@ -0,0 +1,27 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget{
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("TaskManager"),
+      ),
+      body: Column(
+        children: const [
+          Text("Home Screen"),
+        ],
+      ),
+    );
+  }
+
+}
